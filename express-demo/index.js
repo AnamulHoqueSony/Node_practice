026869ad78f2768eff221c4f3e21a0dff22c714c
Anamul_ru <roni.twenty@gmail.com>
2019-09-01 12:35:09 +0600

    CRUD on express

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,7 @@
 const  express = require('express');
+const joi = require('joi');
 const app = express();
-
+app.use(express.json());
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
@@ -16,11 +17,43 @@ app.get('/api/courses',(req,res) =>{
     res.send(courses);
 });
 
-// api /courses
+app.post('/api/courses',(req,res) =>{
+    if(!req.body.name || req.body.name.length < 3){
+        res.status(400).send("XXXXXXXXX");
+    }
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+    return ;
+});
+
+app.put ('/api/courses/:id',(req,res) =>{
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course is not found');
+    if(!req.body.name || req.body.name.length < 3){
+        res.status(400).send("XXXXXXXXX");
+    }
+    course.name = req.body.name;
+    res.send(course);
+});
 
 app.get('/api/courses/:id',(req,res) =>{
     const course = courses.find( c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course is not found');
     res.send(course);
+});
+
+app.delete('/api/courses/:id',(req,res) =>{
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course is not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
 });
 app.listen(3000, () => console.log("Listening on prot 3000 ..."));
