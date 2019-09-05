08cccc1d44229ca8f3890b5c3cdf40e2918a8485
Anamul_ru <roni.twenty@gmail.com>
2019-09-05 17:53:01 +0600

    node more basic

--- restapi/api/routes/products.js
+++ restapi/api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req,res,next) =>{
 });
 
 router.post('/',(req,res,next) => {
+    const product = {
+        name: req.body.name,
+        price:req.body.price
+    };
      res.status(200).json({
-        message:'Handling the post request to products'
+        message:'Handling the post request to products',
+        createdproduct:product
      });
 });
 router.get('/:productId',(req,res,next) =>{
